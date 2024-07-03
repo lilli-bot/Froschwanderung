93cee46e62c9fbecab91ff6f5c596df24c244767
spechticus <schwarzspecht@hotmail.com>
2024-07-03 11:09:52 +0200

    explicitly specify the ports for endpoints

--- Display_app/display_app.py
+++ Display_app/display_app.py
@@ -4,17 +4,20 @@ import redis
 app = Flask(__name__)
 r = redis.Redis(host="localhost", port=6379, db=0)
 
+
 @app.route("/")
 def index():
     counters = r.hgetall("likes")
-    counters = {k.decode('utf-8'): int(v) for k, v in counters.items()}
+    counters = {k.decode("utf-8"): int(v) for k, v in counters.items()}
     return render_template("allfrogs.html", counters=counters)
+
 
 @app.route("/get_counters")
 def get_counters():
     counters = r.hgetall("likes")
-    counters = {k.decode('utf-8'): int(v) for k, v in counters.items()}
+    counters = {k.decode("utf-8"): int(v) for k, v in counters.items()}
     return jsonify(counters=counters)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5002)
