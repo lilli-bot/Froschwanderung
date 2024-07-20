fc77c949a0a7656a234853bbce4c26b32900acc2
Daniel Mueller <schwarzspecht@hotmail.com>
2024-07-20 15:07:02 +0200

    update install script to create and activate a virtual environment

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
+python3 -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
+
 if command -v redis-server &> /dev/null; then
     REDIS_CMD=$(command -v redis-server)
     echo "Redis is already installed: $REDIS_CMD"
