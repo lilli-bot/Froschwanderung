c8f55437d667d0c96f7be6769ae165231ee3575e
spechticus <schwarzspecht@hotmail.com>
2024-08-14 19:29:41 +0200

    implement user_id and session_id columns

---
+++ analytics/macros/logic.sql
@@ -0,0 +1,25 @@
+{%- macro extract_user_from_timestamp(timestamp_column) -%}
+{# After X minutes of idle time between the rows, we assume that a new user has entered #}
+  CASE WHEN 
+    date_sub('minute', 
+              LAG({{timestamp_column}}, 1) OVER (ORDER BY {{timestamp_column}})::TIMESTAMP,
+              {{timestamp_column}}::TIMESTAMP) 
+        > {{var("new_user_after_minutes")}}
+    THEN TRUE
+    ELSE FALSE
+    END
+{%- endmacro -%}
+
+
+{%- macro extract_session_from_timestamp(timestamp_column) -%}
+{# If more than X hours have passed between new evens, we assume that a new session has begun #}
+  CASE WHEN 
+    date_sub('hour', 
+              LAG({{timestamp_column}}, 1) OVER 
+              (ORDER BY {{timestamp_column}})::TIMESTAMP, 
+              {{timestamp_column}}::TIMESTAMP) 
+        > {{var('new_session_after_hours')}}
+    THEN TRUE
+    ELSE FALSE
+    END
+{%- endmacro -%}
--- analytics/models/staging/stg_appanalytics.sql
+++ analytics/models/staging/stg_appanalytics.sql
@@ -1,3 +1,4 @@
+
 {{
   config(
     materialized = 'table',
@@ -7,7 +8,24 @@
 WITH parquet_data AS (
     SELECT *
     FROM read_parquet('./results/*.parquet', filename=true)
+),
+lagged_timestamps AS (
+    SELECT 
+        event_id,
+        clicked_image,
+        not_clicked_image,
+        timestamp,
+        filename,
+        {{extract_session_from_timestamp(timestamp_column = 'timestamp')}} AS session_change,
+        {{extract_user_from_timestamp(timestamp_column = 'timestamp')}} AS user_change
+    FROM parquet_data
 )
-
-SELECT *
-FROM parquet_data
+SELECT
+    event_id,
+    clicked_image,
+    not_clicked_image,
+    timestamp,
+    filename,
+    SUM(CASE WHEN user_change = TRUE THEN 1 ELSE 0 END) OVER (ORDER BY timestamp) + 1 AS user_id,
+    SUM(CASE WHEN session_change = TRUE THEN 1 ELSE 0 END) OVER (ORDER BY timestamp) + 1 AS session_id
+FROM lagged_timestamps
