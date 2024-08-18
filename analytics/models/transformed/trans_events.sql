3d68b2fd12b8a1bfb905d6895642901b69239706
spechticus <schwarzspecht@hotmail.com>
2024-08-19 00:37:32 +0200

    implement first naive version of penalty_factor

--- analytics/models/transformed/trans_events.sql
+++ analytics/models/transformed/trans_events.sql
@@ -1,7 +1,7 @@
-
-SELECT 
+SELECT
     e.*,
-    s.session_title
-FROM {{ ref('stg_events') }} e
-CROSS JOIN {{ ref('sessions') }} s
-    WHERE e.timestamp BETWEEN s.session_start AND s.session_end
+    s.session_title,
+    EXP(-0.3 * (e.current_winner_streak - 1)) AS penalty_factor
+FROM {{ ref('stg_events') }} AS e
+CROSS JOIN {{ ref('sessions') }} AS s
+WHERE e.timestamp BETWEEN s.session_start AND s.session_end
