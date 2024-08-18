3f2873ed179011a52c716d8940e52735dd176ee9
spechticus <schwarzspecht@hotmail.com>
2024-08-18 22:07:31 +0200

    Test strict SQL fluff linting

--- analytics/macros/logic.sql
+++ analytics/macros/logic.sql
@@ -1,10 +1,11 @@
 {%- macro extract_user_from_timestamp(timestamp_column) -%}
-{# After X minutes of idle time between the rows, we assume that a new user has entered #}
+{# After X minutes of idle time between the rows, we assume that a 
+  new user has entered #}
   CASE WHEN 
     date_sub('minute', 
-              LAG({{timestamp_column}}, 1) OVER (ORDER BY {{timestamp_column}})::TIMESTAMP,
-              {{timestamp_column}}::TIMESTAMP) 
-        > {{var("new_user_after_minutes")}}
+              LAG({{ timestamp_column }}, 1) OVER (ORDER BY {{ timestamp_column }})::TIMESTAMP,
+              {{ timestamp_column }}::TIMESTAMP) 
+        > {{ var("new_user_after_minutes") }}
     THEN TRUE
     ELSE FALSE
     END
@@ -12,14 +13,17 @@
 
 
 {%- macro extract_session_from_timestamp(timestamp_column) -%}
-{# If more than X hours have passed between new evens, we assume that a new session has begun #}
+{# If more than X hours have passed between new evens, we assume that a 
+  new session has begun #}
   CASE WHEN 
     date_sub('hour', 
-              LAG({{timestamp_column}}, 1) OVER 
-              (ORDER BY {{timestamp_column}})::TIMESTAMP, 
-              {{timestamp_column}}::TIMESTAMP) 
-        > {{var('new_session_after_hours')}}
+              LAG({{ timestamp_column }}, 1) OVER 
+              (ORDER BY {{ timestamp_column }})::TIMESTAMP, 
+              {{ timestamp_column }}::TIMESTAMP) 
+        > {{ var('new_session_after_hours') }}
     THEN TRUE
     ELSE FALSE
     END
+
 {%- endmacro -%}
+
